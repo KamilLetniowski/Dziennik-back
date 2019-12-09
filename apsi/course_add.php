ce486ea832f4273be671675ec86b60165d5e0127
SpiritusPoland <lkszmalinowski@gmail.com>
2019-12-09 23:32:45 +0100

    edited course_add

--- apsi/course_add.php
+++ apsi/course_add.php
@@ -1,43 +1,27 @@
 <?php
-
 require 'connect.php';
 
+$postdata = file_get_contents("php://input");
 
-echo $leader = $_POST['władca_kurwiszonow'];
-echo $course_name = $_POST['kurwiszon_tortura'];
-
-//$postdata = file_get_contents("php://input");
-//
-//if(isset($postdata) && !empty($postdata))
-//{
-//    // Extract the data.
-//    $request = json_decode($postdata);
-//
-//
-//    // Validate.
-//    if(trim($request->username) === '' || trim($request->pass) === '')
-//    {
-//        return http_response_code(400);
-//    }
-//
-//    // Sanitize.
-//    $name = mysqli_real_escape_string($con, trim($request->username));
-//    $pass = mysqli_real_escape_string($con, trim($request->pass));
+if(isset($postdata) && !empty($postdata)) {
+    // Extract the data.
+    $request = json_decode($postdata);
 
 
-// Store.
-$sql = "INSERT INTO `course`(`name`,`leader`) VALUES ('$course_name','$leader')";
+    // Validate.
+    if (trim($request->cname) === '' || trim($request->cleader) === '') {
+        return http_response_code(400);
+    }
+
+    // Sanitize.
+    $cName = mysqli_real_escape_string($con, trim($request->cname));
+    $cLeader = mysqli_real_escape_string($con, trim($request->cleader));
+
+    $sql = "INSERT INTO `course`(`name`,`leader`) VALUES ('$cName','$cLeader')";
 //    mysqli_query($con,$sql);
-if (mysqli_query($con, $sql)) {
-    http_response_code(201);
-    $user = [
-        'leader' => $leader,
-        'course' => $course_name,
-//            'id'    => mysqli_insert_id($con)
-    ];
-    echo json_encode(['data' => $user]);
-} else {
-    http_response_code(422);
+    if (mysqli_query($con, $sql)) {
+        http_response_code(201);
+    } else {
+        http_response_code(422);
+    }
 }
-//}
-
